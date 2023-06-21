c93fe2349c6103cecf8ec1fda29c2788a4042b28
BrentGruber <brent.gruber77@gmail.com>
2023-06-21 19:23:28 -0400

    adding AddAccountBalance function

--- simplebank/db/sqlc/account_test.go
+++ simplebank/db/sqlc/account_test.go
@@ -68,6 +68,25 @@ func TestUpdateAccount(t *testing.T) {
 	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
 }
 
+func TestAddAccountBalance(t *testing.T) {
+	account1 := createRandomAccount(t)
+
+	arg := AddAccountBalanceParams{
+		ID:     account1.ID,
+		Amount: util.RandomMoney(),
+	}
+
+	account2, err := testQueries.AddAccountBalance(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Balance+arg.Amount, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+}
+
 func TestDeleteAccount(t *testing.T) {
 	account1 := createRandomAccount(t)
 	err := testQueries.DeleteAccount(context.Background(), account1.ID)
