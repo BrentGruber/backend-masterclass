55d2dfb90e723c5a96becece2d6a140824aebbef
BrentGruber <brent.gruber77@gmail.com>
2023-06-21 23:56:22 -0400

    using viper for config management

--- main.go
+++ main.go
@@ -6,17 +6,17 @@ import (
 
 	"github.com/BrentGruber/simplebank/api"
 	db "github.com/BrentGruber/simplebank/db/sqlc"
+	"github.com/BrentGruber/simplebank/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://postgres:postgres@localhost:5432/simplebank?sslmode=disable"
-	serverAddress = "0.0.0.0:9000"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load configuration:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -24,7 +24,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
